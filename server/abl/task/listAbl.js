2887532a4da7eb692dde76c9f4c970990cf75690
Jakub Drbohlav <jakub.drbohlav@firma.seznam.cz>
2024-04-29 12:44:05 +0200

    feat: task list endpoint

--- server/abl/task/listAbl.js
+++ server/abl/task/listAbl.js
@@ -1,6 +1,47 @@
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats").default;
+const ajv = new Ajv();
+addFormats(ajv);
+
+const taskDao = require("../../dao/task-dao");
+
+const schema = {
+  type: "object",
+  properties: {
+    done: { type: "boolean" },
+    tasklistId: { type: "string" },
+    deadlineUntil: {
+      type: "string",
+      format: "date",
+      pattern: "^\\d{4}-\\d{2}-\\d{2}$",
+    },
+  },
+  required: [],
+  additionalProperties: false,
+};
+
 async function ListAbl(req, res) {
   try {
-    res.send("/task/list endpoint");
+    const filters = req.body;
+
+    // Validate input
+    const valid = ajv.validate(schema, filters);
+    if (!valid) {
+      res.status(400).json({
+        code: "invalidDtoIn",
+        message: "DtoIn is not valid.",
+        validationError: ajv.errors,
+      });
+      return;
+    }
+
+    const tasks = taskDao.list(
+      filters.done,
+      filters.tasklistId,
+      filters.deadlineUntil
+    );
+
+    res.send({ tasks });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
--- server/dao/task-dao.js
+++ server/dao/task-dao.js
@@ -18,6 +18,50 @@ function get(id) {
   }
 }
 
+// Gets all tasks that meet specified filters
+function list(done, tasklistId, deadlineUntil) {
+  try {
+    const files = fs.readdirSync(TASK_FOLDER_PATH);
+    let tasks = files.map((file) => {
+      const fileData = fs.readFileSync(
+        path.join(TASKLIST_FOLDER_PATH, file),
+        "utf8"
+      );
+      return JSON.parse(fileData);
+    });
+
+    // Filter done state
+    if (typeof done === "boolean") {
+      tasks = tasks.filter((a) => a.done === done);
+    }
+
+    // Filter tasklist ID
+    if (tasklistId !== undefined) {
+      tasks = tasks.filter((a) => a.tasklistId === tasklistId);
+    }
+
+    // Filter deadline
+    if (deadlineUntil) {
+      const maxDate = new Date(deadlineUntil);
+      tasks = tasks.filter((a) => {
+        new Date(a.deadline) <= maxDate;
+      });
+    }
+
+    // Sort by date ascending
+    tasks.sort((a, b) => {
+      const dateA = new Date(a.deadline);
+      const dateB = new Date(b.deadline);
+      return dateA - dateB;
+    });
+
+    return tasks;
+  } catch (error) {
+    throw { code: "failedToListTasks", message: error.message };
+  }
+}
+
 module.exports = {
   get,
+  list,
 };
